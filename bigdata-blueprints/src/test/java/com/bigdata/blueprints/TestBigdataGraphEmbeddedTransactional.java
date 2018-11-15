2020ca53e20039804b02cc3e93d1aa503de5e183
Ian Dunlop <ianwdunlop@gmail.com>
2018-11-15 15:37:55 +0000

    Test was expecting an Int not a String
    (cherry picked from commit 65be7f89e51975c7bf5dbcf49050575b4fa08b25)

--- bigdata-blueprints/src/test/java/com/bigdata/blueprints/TestBigdataGraphEmbeddedTransactional.java
+++ bigdata-blueprints/src/test/java/com/bigdata/blueprints/TestBigdataGraphEmbeddedTransactional.java
@@ -127,8 +127,8 @@ public class TestBigdataGraphEmbeddedTransactional extends AbstractTestBigdataGr
                     v1.setProperty("key2", 10);
                     v2.setProperty("key2", 20);
 
-                    assertEquals(10, v1.getProperty("key2"));
-                    assertEquals(20, v2.getProperty("key2"));
+                    assertEquals(10, Integer.parseInt(v1.getProperty("key2")));
+                    assertEquals(20, Integer.parseInt(v2.getProperty("key2")));
                 }
 
             }
